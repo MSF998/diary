7052c3cc8069c51883cb66fbeb769c88e28a027d
msf998 <mohdsahnoon@gmail.com>
2025-04-06 11:55:11 +0530

    fixed job details for first 10 jobs

--- routes/jobPostings.py
+++ routes/jobPostings.py
@@ -7,7 +7,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
-def scrape_amazon_jobs(query="python", max_jobs=10):
+def scrape_amazon_jobs(query="python"):
     url = f"https://www.amazon.jobs/en/search?base_query={query}&industry_experience=one_to_three_years&country%5B%5D=IND&state%5B%5D=Karnataka"
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
@@ -22,27 +22,28 @@ def scrape_amazon_jobs(query="python", max_jobs=10):
         WebDriverWait(driver, 10).until(
             EC.presence_of_element_located((By.XPATH, xpath))
         )
-        print("Found the target div!")
         soup = BeautifulSoup(driver.page_source, 'html.parser')
         container = soup.find('div', class_=['job-tile-lists'])
-        container = soup.find('div',class_=['job-tile'])
-        container = soup.find('div',class_=['job'])
-        
-        
-        for element in container.find_all(class_=True)[:1]:
-            print("++++++++++++++++++++++++++")
-            contents = element.contents
-            contents = contents[0]
-            href = contents.find('h3').find('a')['href']
-            print(href)
-            details_div = contents.find('div').find('span').find('ul').find_all('li')
-            location = details_div[0].string
-            job_id = details_div[-1].string
-            job_id = job_id.split(':')[1]
-            job_id = int(job_id.strip())
-            print(location,job_id)
-            print("++++++++++++++++++++++++++")
-        return "Hello"
+        job_details = []
+        for product_div in soup.find_all('div', class_='job-tile'):
+            container = product_div.find('div',class_=['job'])
+            
+            for element in container.find_all(class_=True)[:1]:
+                details:dict = {}
+                contents = element.contents
+                contents = contents[0]
+                href = contents.find('h3').find('a')['href']
+                title = href.split('/')[-1].replace("-"," ")
+                details_div = contents.find('div').find('span').find('ul').find_all('li')
+                location = details_div[0].string
+                job_id = details_div[-1].string
+                job_id = job_id.split(':')[1]
+                job_id = int(job_id.strip())
+                details.update({"job_id":job_id, "title":title, "location":location, "href":href})
+                job_details.append(details)
+
+        return job_details
+
     except Exception as e:
         print(e)
         return {"error": str(e)}
@@ -54,17 +55,18 @@ job_postings_bp = Blueprint('job_postings', __name__)
 
 @job_postings_bp.route('/job-postings')
 def job_postings():
-    job_urls = scrape_amazon_jobs()
+    job_details = scrape_amazon_jobs()
     #Aggregate job postings from various sites
     """
-    Product Based or Service Based
     Company Name
     Position Name
     Date posted
     YOE Required
     Location
     """
-    return render_template('jobPostings.html',job_urls=job_urls)
+    print(job_details)
+    return jsonify(job_details)
+    # return render_template('jobPostings.html',job_urls=job_urls)
 
 
 #TODO:Scrape Jon Postings Data from Amazon, Microsoft, IBM, Cognizant, Concentrix
